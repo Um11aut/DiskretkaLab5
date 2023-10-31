f23f6c1c82d48ed4a9e32d8dd9c2a7e36829b0de
Um11aut <144227643+Um11aut@users.noreply.github.com>
2023-10-31 22:26:58 +0200

    Добавьте файлы проекта.

---
+++ BooleanFunction.hpp
@@ -0,0 +1,253 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class BooleanFunction {
+public:
+	explicit BooleanFunction(const std::string value) : m_value(value) {}
+
+	void print() {
+		size_t index = 0;
+
+		for (int x = 0; x <= 1; ++x) {
+			for (int y = 0; y <= 1; ++y) {
+				for (int z = 0; z <= 1; ++z) {
+					std::cout << x << ' ' << y << ' ' << z << ' ' << m_value[index] << std::endl;
+					index++;
+				}
+			}
+		}
+	}
+
+	char& operator[](const int element) {
+		return m_value[element];
+	}
+
+	bool operator==(const BooleanFunction& other) {
+		if (m_value == other.m_value) {
+			return true;
+		}
+
+		return false;
+	}
+
+	BooleanFunction findDouble() {
+		BooleanFunction temp(m_value);
+
+		for (size_t i = 0; i < m_value.size(); ++i) {
+			bool val = (m_value[i] == '1');
+			temp[i] = static_cast<char>(!val + '0');
+		}
+
+		temp.reverse();
+
+		return temp;
+	}
+
+	bool isSelfDoubled() {
+		BooleanFunction temp = findDouble();
+		if (this->m_value == temp.m_value) {
+			return true;
+		}
+
+		return false;
+	}
+
+	void printDDNF() {
+		std::string result = "";
+
+		size_t index = 0;
+
+		for (int x = 0; x <= 1; ++x) {
+			for (int y = 0; y <= 1; ++y) {
+				for (int z = 0; z <= 1; ++z) {
+					if (m_value[index] == '1') {
+						result += "(";
+						std::string part = "";
+
+						if (x == 1) { part += " x &&"; } else { part += " -x &&"; }
+						if (y == 1) { part += " y &&"; } else { part += " -y &&"; }
+						if (z == 1) { part += " z &&"; } else { part += " -z &&"; }
+
+						part = part.substr(0, part.length() - 2);
+
+						result += part;
+						result += ") V ";
+					}
+					index++;
+				}
+			}
+		}
+		result = result.substr(0, result.length() - 3);
+		std::cout << result;
+	}
+
+	void printGegalkin() {
+		std::string result = "";
+
+		size_t index = 0;
+
+		for (int x = 0; x <= 1; ++x) {
+			for (int y = 0; y <= 1; ++y) {
+				for (int z = 0; z <= 1; ++z) {
+					if (m_value[index] == '1') {
+						result += "(";
+						std::string part = "";
+
+						if (x == 0) { part += " (x XOR 1) Ʌ "; } else { part += " x Ʌ  "; }
+						if (y == 0) { part += " (y XOR 1) Ʌ "; } else { part += " y Ʌ  "; }
+						if (z == 0) { part += " (z XOR 1) Ʌ "; } else { part += " z Ʌ  "; }
+						
+						part = part.substr(0, part.length() - 4);
+
+						result += part;
+						result += ") XOR ";
+						
+					}
+					index++;
+				}
+			}
+		}
+		result = result.substr(0, result.length() - 4);
+		std::cout << result;
+	}
+
+	void printDKNF() {
+		std::string result = "";
+
+		size_t index = 0;
+
+		for (int x = 0; x <= 1; ++x) {
+			for (int y = 0; y <= 1; ++y) {
+				for (int z = 0; z <= 1; ++z) {
+					if (m_value[index] == '0') {
+						result += "(";
+						std::string part = "";
+
+						if (x == 0) { part += " x &&"; } else { part += " -x &&"; }
+						if (y == 0) { part += " y &&"; } else { part += " -y &&"; }
+						if (z == 0) { part += " z &&"; } else { part += " -z &&"; }
+
+						part = part.substr(0, part.length() - 2);
+
+						result+=part;
+						result += ") Ʌ ";
+					}
+					index++;
+				}
+			}
+		}
+		result = result.substr(0, result.length() - 3);
+		std::cout << result;
+	}
+
+	bool remainsConstant(bool constant) {
+		size_t index = 0;
+
+		for (int x = 0; x <= 1; ++x) {
+			for (int y = 0; y <= 1; ++y) {
+				for (int z = 0; z <= 1; ++z) {
+					if (constant) {
+						if (x == 1 && y == 1 && z == 1 && m_value[index] == '1') {
+							return true;
+						}
+					}
+					else if(!constant) {
+						if (x == 0 && y == 0 && z == 0 && m_value[index] == '0') {
+							return true;
+						}
+					}
+					index++;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	bool isMonotonic() {
+		size_t index = 0;
+
+		for (int x = 0; x <= 1; ++x) {
+			for (int y = 0; y <= 1; ++y) {
+				for (int z = 0; z <= 1; ++z) {
+					if (x == 1 && y == 1 && z == 1) continue;
+					std::string base = std::to_string(x) + std::to_string(y) + std::to_string(z);
+
+					for (size_t i = 0; i < base.size(); ++i) {
+						if (base[i] == '0') {
+							int el = base[i] - '0';
+							base[i] = (char)(++el + '0');
+
+							if (m_value[binaryToDecimal(base)] - '0' < m_value[index] - '0' && m_value[binaryToDecimal(base)] == '1') {
+								return false;
+							}
+						}
+					}
+
+					index++;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	bool isLinear() {
+		size_t index = 0;
+
+		std::vector<std::string> result;
+
+		for (int x = 0; x <= 1; ++x) {
+			for (int y = 0; y <= 1; ++y) {
+				for (int z = 0; z <= 1; ++z) {
+					if (m_value[index] == '1') {
+						if (x == 0 && y == 0 && z == 0) result.push_back("1");
+
+						std::string temp;
+
+						if (x == 1) temp += "x";
+						if (y == 1) temp += "y";
+						if (z == 1) temp += "z";
+
+						if (temp.size() > 1) return false;
+
+						if(!temp.empty()) result.push_back(temp);
+					}
+					index++;
+				}
+			}
+		}
+
+		for (const auto& el : result) {
+			std::cout << el << " + ";
+		}
+		return true;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const BooleanFunction& func) {
+		os << func.m_value;
+
+		return os;
+	}
+private:
+
+	inline void reverse() {
+		std::reverse(m_value.begin(), m_value.end());
+	}
+
+	int binaryToDecimal(std::string binary) {
+		int decimal = 0;
+		int binaryLength = binary.length();
+
+		for (int i = 0; i < binaryLength; ++i) {
+			if (binary[i] == '1') {
+				decimal += pow(2, binaryLength - 1 - i);
+			}
+		}
+
+		return decimal;
+	}
+
+	std::string m_value;
+};
---
+++ DiskretkaLab5.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include "BooleanFunction.hpp"
+
+int main()
+{
+    BooleanFunction func{"00010111"};
+
+    func.print();
+    func.printDDNF();
+    std::cout << std::endl;
+    func.printDKNF();
+    std::cout << std::endl;
+    func.printGegalkin();
+
+    std::cout << std::endl;
+
+    auto temp = func.findDouble();
+
+
+    if (func.remainsConstant(true)) {
+        std::cout << "зберігає константу 1" << std::endl;
+    }
+    if (func.remainsConstant(false)) {
+        std::cout << "зберігає константу 0" << std::endl;
+    }
+    if (func.isSelfDoubled()) {
+        std::cout << "функція самодвоїста" << std::endl;
+    }
+    if (func.isMonotonic()) {
+        std::cout << "функція монотонна" << std::endl;
+    }
+    if (func.isLinear()) {
+        std::cout << "функція лінійна" << std::endl;
+    }
+}
